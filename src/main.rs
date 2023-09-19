a9ab84b8b094da1b12c33a66da1b9f7e739406b3
simmihugs <simon.graetz@gmx.de>
2023-09-19 15:48:57 +0200

    Added second example

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,116 @@
-use crossterm::{
-    cursor,
-    style::{self, Stylize},
-    terminal, ExecutableCommand, QueueableCommand,
+use crossterm::event::{
+    poll, KeyboardEnhancementFlags, PopKeyboardEnhancementFlags, PushKeyboardEnhancementFlags,
 };
-use std::io::{self, Write};
+use crossterm::{
+    cursor::position,
+    event::{
+        read, DisableBracketedPaste, DisableFocusChange, DisableMouseCapture, EnableBracketedPaste,
+        EnableFocusChange, EnableMouseCapture, Event, KeyCode,
+    },
+    execute, queue,
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
+use std::io;
+use std::time::Duration;
 
-fn main() -> io::Result<()> {
-    let mut stdout = io::stdout();
+/**
+ * This is how to make a multiline comment...
+ * asdfasd
+ * asdfsad
+ * asdfas
+ * df
+ * dsaf
+ */
+const HELP: &str = r#"Blocking read()
+ - Keyboard, mouse, focus and terminal resize events enabled
+ - Hit "c" to print current cursor position
+ - Use Esc to quit
+"#;
 
-    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+fn print_events() -> io::Result<()> {
+    loop {
+        // Blocking read
+        let event = read()?;
 
-    for y in 0..10 {
-        for x in 0..50 {
-            if (y == 0 || y == 10 - 1) || (x == 0 || x == 50 - 1) {
-                // in this loop we are more efficient by not flushing the buffer.
-                stdout
-                    .queue(cursor::MoveTo(x, y))?
-                    .queue(style::PrintStyledContent("█".magenta()))?;
-            }
+        println!("Event: {:?}\r", event);
+
+        if event == Event::Key(KeyCode::Char('c').into()) {
+            println!("Cursor position: {:?}\r", position());
+        }
+
+        if let Event::Resize(x, y) = event {
+            let (original_size, new_size) = flush_resize_events((x, y));
+            println!("Resize from: {:?}, to: {:?}\r", original_size, new_size);
+        }
+
+        if event == Event::Key(KeyCode::Esc.into()) {
+            break;
         }
     }
-    stdout.flush()?;
+
     Ok(())
+}
+
+// Resize events can occur in batches.
+// With a simple loop they can be flushed.
+// This function will keep the first and last resize event.
+fn flush_resize_events(first_resize: (u16, u16)) -> ((u16, u16), (u16, u16)) {
+    let mut last_resize = first_resize;
+    while let Ok(true) = poll(Duration::from_millis(50)) {
+        if let Ok(Event::Resize(x, y)) = read() {
+            last_resize = (x, y);
+        }
+    }
+
+    (first_resize, last_resize)
+}
+
+fn main() -> io::Result<()> {
+    println!("{}", HELP);
+
+    enable_raw_mode()?;
+
+    let mut stdout = io::stdout();
+
+    let supports_keyboard_enhancement = matches!(
+        crossterm::terminal::supports_keyboard_enhancement(),
+        Ok(true)
+    );
+
+    if supports_keyboard_enhancement {
+        queue!(
+            stdout,
+            PushKeyboardEnhancementFlags(
+                KeyboardEnhancementFlags::DISAMBIGUATE_ESCAPE_CODES
+                    | KeyboardEnhancementFlags::REPORT_ALL_KEYS_AS_ESCAPE_CODES
+                    | KeyboardEnhancementFlags::REPORT_ALTERNATE_KEYS
+                    | KeyboardEnhancementFlags::REPORT_EVENT_TYPES
+            )
+        )?;
+    }
+
+    execute!(
+        stdout,
+        EnableBracketedPaste,
+        EnableFocusChange,
+        EnableMouseCapture,
+    )?;
+
+    if let Err(e) = print_events() {
+        println!("Error: {:?}\r", e);
+    }
+
+    if supports_keyboard_enhancement {
+        queue!(stdout, PopKeyboardEnhancementFlags)?;
+    }
+
+    execute!(
+        stdout,
+        DisableBracketedPaste,
+        PopKeyboardEnhancementFlags,
+        DisableFocusChange,
+        DisableMouseCapture
+    )?;
+
+    disable_raw_mode()
 }
